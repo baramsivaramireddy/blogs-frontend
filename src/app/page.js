1bcd9b14a503318db638842e8323f3eb71014654
baramsivaramireddy <baramsivarami@gmail.com>
2024-03-26 08:35:52 +0530

    create a blog button

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 "use client"
 
 import DisplayBlogs from "@/components/pages/shared/displayBlogs";
+import { useToken } from "@/hooks/useAuth";
+import Link from "next/link";
 export default function Home() {
+
+  const token = useToken()
   return (
-    <div className="  h-full p-5 ">
+    <div className="  h-full p-5  flex flex-col gap-3">
+
+
         <h1 className="text-2xl font-bold  my-5"> BLOGS</h1>
         
+
+        { token &&  <Link className='action-button w-48 ' href={`/dashboard/blog/create`} >create a blog</Link>}
+
         <DisplayBlogs />
 
     </div>
